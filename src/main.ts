f9be07e730c0f9acef7bacca7c8f292cb6b656d9
drey123 <101038729+drey123@users.noreply.github.com>
2026-08-14 11:09:53 +0100

    Let Vite bundle application CSS from TypeScript entrypoint

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+import './style.css';
+import './DirectionalPad.css';
+import './InteractionMenu.css';
 import { initInputs } from './Interface/InteractionMenu';
 import { initScaling } from './scaling';
-import { initThree } from './three/threeLoader'
+import { initThree } from './three/threeLoader';
 
 initInputs();
-
 initThree();
-
 initScaling();
